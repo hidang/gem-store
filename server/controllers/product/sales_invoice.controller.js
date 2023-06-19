e72fc29db6864116bd0be0fc9c904ecfb49b1271
hidang <yellowstudio.vn@gmail.com>
2023-06-20 01:54:59 +0700

    feat: api sales invoice

--- server/controllers/product/sales_invoice.controller.js
+++ server/controllers/product/sales_invoice.controller.js
@@ -1,15 +1,70 @@
 const db = require('../../models');
 const SalesInvoices = db.SalesInvoices;
 const ProductOnSales = db.ProductOnSales;
+const ProductTypes = db.ProductTypes;
+const Products = db.Products;
+const Units = db.Units;
+
 const Op = db.Sequelize.Op;
 
+async function createProductOnSales(idSaleInvoice, idd, countt) {
+  const dataProduct = await Products.findByPk(idd).catch((err) => {
+    console.log('Lỗi xảy ra product_id=' + idd + ' ' + err.message);
+    return;
+  });
+  console.log('@@XX product: ', dataProduct);
+
+  if (dataProduct) {
+    console.log('@@XX product: ', dataProduct);
+    const productType = await ProductTypes.findByPk(dataProduct.productType_id).catch((err) => {
+      console.log(`Error retrieving ProductType with id=' + dataProduct.productType_id.`);
+      return;
+    });
+    if (productType) {
+    } else {
+      console.log(`Cannot find ProductType with id=${dataProduct.productType_id}.`);
+    }
+    const unit = await Units.findByPk(productType.unit_id).catch((err) => {
+      console.log(`Error retrieving Unit with id=${productType.unit_id}.`);
+      return;
+    });
+
+    // Create a ProductOnSale
+    const productOnSale = {
+      salesInvoice_id: idSaleInvoice,
+      id: idd,
+      count: countt,
+      // must have info on create
+      productTypeName: productType.name,
+      profitPercent: productType.profitPercent,
+      unitName: unit.name,
+
+      // TODO: check count maximum
+      // fetch in there
+      name: dataProduct.name,
+      pricePerProduct: dataProduct.pricePerProduct,
+      massPerProduct: dataProduct.massPerProduct,
+      supplier_id: dataProduct.supplier_id,
+      purchaseInvoice_id: dataProduct.purchaseInvoice_id
+    };
+
+    console.debug('### Create ProductOnSale: ', productOnSale);
+
+    // Save Product in the database
+    await ProductOnSales.create(productOnSale).catch((err) => {
+      console.log(err.message || 'Some error occurred while creating the Product.');
+    });
+  } else {
+    console.log(`Cannot find ProductType with id=${dataProduct.productType_id}.`);
+  }
+}
 // Create and Save a new SalesInvoice
 exports.create = (req, res) => {
   // Validate request
   const body = req.body;
-  if (!body.customer_id) {
+  if (!body.customer_id || !body.products) {
     res.status(400).send({
-      message: 'customer_id can not be empty!'
+      message: 'Content customer_id || products[] can not be empty!'
     });
     return;
   }
@@ -23,8 +78,14 @@ exports.create = (req, res) => {
 
   // Save SalesInvoice in the database
   SalesInvoices.create(salesInvoice)
-    .then((data) => {
-      res.send(data);
+    .then(async (salesInvoice) => {
+      // create products on sale
+      for (const product of body.products) {
+        await createProductOnSales(salesInvoice.id, product.id, product.count);
+      }
+      const result = await SalesInvoices.findByPk(salesInvoice.id, { include: ['productOnSales'] });
+
+      res.send(result);
     })
     .catch((err) => {
       res.status(500).send({
